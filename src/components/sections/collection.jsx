3728fe258817d0071cc12b814ecfe97580b75e80
Jamshidxushnazarov <xushnazarovjamshid927@gmail.com>
2025-04-10 01:56:38 +0500

    collection section complited

--- src/components/sections/collection.jsx
+++ src/components/sections/collection.jsx
@@ -1,11 +1,109 @@
-import React from 'react'
+import money from "../../assets/images/Allcollection_money.svg";
+import nft from "../../assets/images/Allcollection_NFT.svg";
+import transit from "../../assets/images/Allcollection_transit.svg";
+import { cards } from "../../utils/collectionData";
+import { useState } from "react";
+
+
 
 const Collection = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const [cardStates, setCardStates] = useState(cards);
 
-export default Collection
+    const toggleLike = (id) => {
+        const updatedCards = cardStates.map((card) =>
+            card.id === id ? { ...card, liked: !card.liked } : card
+        );
+        setCardStates(updatedCards);
+    };
+    return (
+        <>
+            <div className='container'>
+                <div>
+                    <h2 className='text-white text-center text-3xl font-bold mt-[120px]'>
+                        All Collection
+                    </h2>
+                    <p className='text-[#888888]  text-center w-[323px] mt-[10px] mx-auto'>
+                        Worlds First Layer 2 Fashion Marketplace
+                    </p>
+                    <div className='flex mt-[75px] max-w-[941px] justify-between mx-auto '>
+                        <div className='flex flex-col items-center'>
+                            <img src={money} alt='money-icon' />
+                            <p className='text-white'>No Gas Fees</p>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <img src={nft} alt='nft-icon' />
+                            <p className='text-white'>Carbon Netural NFTs</p>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <img src={transit} alt='transit-icon' />
+                            <p className='text-white'>
+                                Fast And Easy Transactions
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex flex-wrap justify-between gap-4 pt-[162px]'>
+                    <button className='bg-[#6C5DD3] text-white px-10 py-1 rounded-lg'>
+                        All Collections
+                    </button>
+                    <button className='bg-[#6C5DD3] text-white px-10 py-1 rounded-lg'>
+                        Verified Brands
+                    </button>
+                    <button className='bg-[#6C5DD3] text-white px-10 py-1 rounded-lg'>
+                        Verified Artists
+                    </button>
+                    <button className='bg-[#6C5DD3] text-white px-10 py-1 rounded-lg'>
+                        New Drops
+                    </button>
+                    <button className='bg-[#6C5DD3] text-white px-10 py-1 rounded-lg'>
+                        Live Shows
+                    </button>
+                </div>
+
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pt-6'>
+                    {cardStates.map((card) => (
+                        <div
+                            key={card.id}
+                            className='bg-gradient-to-b from-[#2e033e] to-[#3b0d57] rounded-xl p-6 overflow-hidden shadow-md text-white'>
+                            <img
+                                src={card.image}
+                                alt={card.title}
+                                className='w-full object-cover'
+                            />
+
+                            <div className='pt-4 flex flex-col justify-between'>
+                                <div className='flex justify-between text-sm text-gray-400'>
+                                    <span>{card.author}</span>
+                                    <span>Current Bid</span>
+                                </div>
+
+                                <div className='flex justify-between items-center mt-1'>
+                                    <p className='text-white font-semibold'>
+                                        {card.title}
+                                    </p>
+                                    <p className='text-white font-medium'>
+                                        {card.currentBid}
+                                    </p>
+                                </div>
+
+                                <div className='mt-4 flex justify-between items-center'>
+                                    <button
+                                        className='text-xl'
+                                        onClick={() => toggleLike(card.id)}>
+                                        {card.liked ? "❤️" : "🤍"}
+                                    </button>
+                                    <button className=' bg-[#6C5DD3] text-white px-4 py-2 rounded-lg font-semibold w-[250px]'>
+                                        {card.buttonText}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Collection;
---
+++ src/utils/collectionData.js
@@ -0,0 +1,93 @@
+import collection1 from "../assets/images/AllCollection_img1.png"
+import collection2 from "../assets/images/AllCollection_img2.png";
+import collection3 from "../assets/images/AllCollection_img3.png";
+import collection4 from "../assets/images/AllCollection_img4.png";
+import collection5 from "../assets/images/AllCollection_img5.png";
+import collection6 from "../assets/images/AllCollection_img6.png";
+import collection7 from "../assets/images/AllCollection_img7.png";
+import collection8 from "../assets/images/AllCollection_img8.png";
+import collection9 from "../assets/images/AllCollection_img9.png";
+
+export const cards = [
+    {
+        id: 1,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image:  collection1 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+    {
+        id: 2,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image:  collection2 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+    {
+        id: 3,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image:  collection3 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+    {
+        id: 4,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image:  collection4 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+    {
+        id: 5,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image: collection5 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+    {
+        id: 6,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image:  collection6 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+    {
+        id: 7,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image:  collection7 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+    {
+        id: 8,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image:  collection8 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+    {
+        id: 9,
+        author: "@Johny",
+        title: "Lorem Ipsum",
+        image:  collection9 ,
+        currentBid: "0.005 ETH",
+        liked: false,
+        buttonText: "Buy Now",
+    },
+];
